2095f02c17d787d001caab5ad43f52860f5fe1b0
yogesh shelke <shelkeyogesh874@gmail.com>
2025-01-29 14:21:04 +0530

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173" || " http://localhost:5174",
+    origin: true,
     credentials: true
 }))
 
